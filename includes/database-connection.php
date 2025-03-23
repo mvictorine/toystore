0ba935b3d9598cb817a845f1350fc4966bce25d8
mvictorine <mvictorine@uri.edu>
2025-03-23 19:33:30 -0400

    Update database-connection.php

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -2,11 +2,11 @@
 
 $type     = 'mysql';                             // Type of database
 $server   = '192.185.2.183';                    // Server the database is on
-$db       = 'mvictorine_toystore';     // Name of the database
+$db       = 'mvictori_toystore';     // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'mvictorine_mvictorine';     // Enter YOUR cPanel username and user here
+$username = 'mvictori_mvictorine';     // Enter YOUR cPanel username and user here
 $password = '81WidowSweets';           // Enter YOUR user password here
 
 
